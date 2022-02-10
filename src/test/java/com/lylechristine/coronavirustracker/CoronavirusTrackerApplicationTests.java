c0d9146247a100bb816920d7f6efbbf3fba85a50
Lylio <cxz@gmx.com>
2022-02-10 15:24:13 +0000

    logging added to tests

--- src/test/java/com/lylechristine/coronavirustracker/CoronavirusTrackerApplicationTests.java
+++ src/test/java/com/lylechristine/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -1,12 +1,15 @@
 package com.lylechristine.coronavirustracker;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.lylechristine.coronavirustracker.controllers.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
 class CoronavirusTrackerApplicationTests {
@@ -14,25 +17,34 @@ class CoronavirusTrackerApplicationTests {
     @Autowired
     private HomeController homeController;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoronavirusTrackerApplicationTests.class);
+
     @Test
     void contextLoads() {
         assertThat(homeController).isNotNull();
     }
 
-    //Two dummy tests for configuring Jenkins
+    // Dummy tests for configuring Jenkins
     @Test
     void dummyTest1() {
         int x = 88;
         int y = 88;
-
         assertEquals(x, y);
+        LOGGER.info("*** DUMMY TEST 1 PASSED ***");
     }
 
     @Test
     void dummyTest2() {
         int a = 900;
-        int b = 900;
+        int b = 5001;
+        assertNotEquals(a, b);
+        LOGGER.info("*** DUMMY TEST 2 PASSED ***");
+    }
 
-        assertEquals(a, b);
+    @Test
+    void dummyTest3() {
+        Boolean valueIsFalse = false;
+        assertFalse(valueIsFalse);
+        LOGGER.info("*** DUMMY TEST 3 PASSED ***");
     }
 }
